211f955c4a376f12731ec35fb73e50826464e033
Tim van Boxtel <tim@vanboxtel.ca>
2015-06-11 09:41:02 -0400

    Project specced out

---
+++ sines.py
@@ -0,0 +1,36 @@
+"""
+ The main File of this project
+
+ The specs of this project are as follows:
+
+ 1.  It will be a script which will parse some options
+ 2.  The script will plot some sine waves, variable number
+ 3.  Optionally, we can save the plot to a file of many formats
+ 4.  Script should check that the validity of the arguments
+ 5.  Add an argument for a legend (-l)
+
+usage:
+
+    python sines.py -n 3 -l -f file.png
+"""
+
+def check_args(options):
+    """ 4. Return a Boolean check of the arguments"""
+    pass
+
+def sine_plot(n):
+    """ 2. This plots the sine waves and returns an axis object"""
+    pass
+
+def main(options):
+    """ The main function 
+
+    3.  Plot to a file
+    5.  Add a legend"""
+    pass
+
+
+if __name__ == '__main__':
+    # 1. parse options
+    main(options)
+
